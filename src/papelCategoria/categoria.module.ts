462cdfc4705de4733e9c8af5e0180771d25554fd
famorim-dev <255582023@eniac.edu.br>
2026-03-05 16:35:08 -0300

    fix

--- src/papelCategoria/categoria.module.ts
+++ src/papelCategoria/categoria.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PapelCategoriaController } from './categoria.controller';
 import { PapelCategoriaService } from './categoria.service';
+import { SqlValidaService } from './sqlValida.service';
 
 @Module({
   imports: [],
   controllers: [PapelCategoriaController],
-  providers: [PapelCategoriaService],
+  providers: [PapelCategoriaService, SqlValidaService],
 })
 export class PapelCategoriaModule {}
--- src/papelCategoria/categoria.service.ts
+++ src/papelCategoria/categoria.service.ts
@@ -1,17 +1,17 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
 import { SqlValidaService } from "./sqlValida.service";
 import { getPool } from "src/database/pool";
 import { GerarExcelDto } from "./dto/gerarExcel.dto";
+import prisma from "prisma/con";
 
 
 
 @Injectable()
 export class PapelCategoriaService{
-    constructor(private readonly prisma: PrismaClient, private readonly validaSql: SqlValidaService){}
+    constructor(private readonly validaSql: SqlValidaService){}
 
     async gerarExcel(body: GerarExcelDto, id: string){
-        const registro = await this.prisma.con.findUnique({where: {id: id}})
+        const registro = await prisma.con.findUnique({where: {id: id}})
 
         if(!registro){
             throw new BadRequestException("Id Não encontrado")
