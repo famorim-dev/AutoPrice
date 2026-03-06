12112022bff09faa880fac97be69205c483f8231
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-03-06 11:56:54 -0300

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filter/exceptionsGlobal';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter)
-
+  app.use(helmet())
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Api Rodando na Porta ${process.env.PORT ?? 3000}`)
 }
