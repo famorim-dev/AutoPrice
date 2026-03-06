a2415975995dfffa94dfaf72c630272b78481ee0
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-03-06 11:25:03 -0300

    limit rate add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PapelCategoriaModule } from './papelCategoria/categoria.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core'
 
 @Module({
-  imports: [PapelCategoriaModule],
+  imports: [ThrottlerModule.forRoot([{ttl: 60000,limit: 80,}]),
+    PapelCategoriaModule
+  ],
   controllers: [],
-  providers: [],
+  providers: [{provide: APP_GUARD, useClass: ThrottlerGuard,},
+
+  ],
 })
 export class AppModule {}
