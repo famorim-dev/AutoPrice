64a819aeb5bf83ab8b4b82a46f7b084b935ab388
Felipe Santos de Amorim <255582023@eniac.edu.br>
2026-03-06 12:16:40 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filter/exceptionsGlobal';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter)
   app.use(helmet())
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true,}),)
+  app.enableCors({origin: ['https://devPrice.com', ], methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], credentials: true,})
+  
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Api Rodando na Porta ${process.env.PORT ?? 3000}`)
 }
