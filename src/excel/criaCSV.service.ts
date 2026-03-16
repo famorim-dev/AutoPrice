8021ae7ded8060e675b82ad01d6dee5c8e8345f8
famorim-dev <255582023@eniac.edu.br>
2026-03-16 10:45:49 -0300

    gerando excel a cada 100k linhas

--- src/excel/criaCSV.service.ts
+++ src/excel/criaCSV.service.ts
@@ -10,25 +10,42 @@ export class CsvService {
   async criaCsvEmPasta(dados: Readable, pasta: string, nomeArquivo: string) {
 
     mkdirSync(pasta, { recursive: true });
-    const arquivo = path.join(pasta, nomeArquivo);
+    let parte = 1
+    let linhas = 0
+    const LIMITE = 100000
+    let arquivo = path.join(pasta, `${nomeArquivo}_${parte}.xlsx`)
 
-    const workbook = new ExcelJS.stream.xlsx.WorkbookWriter({ filename: arquivo });
-    const sheet = workbook.addWorksheet('Relatório');
+    let workbook = new ExcelJS.stream.xlsx.WorkbookWriter({ filename: arquivo })
+    let sheet = workbook.addWorksheet('Relatório')
 
-    let headersSet = false;
+    let headersSet = false
+    let headerCols: any
 
     for await (const row of dados) {
 
       if (!headersSet) {
-        const headerCols = Object.keys(row).map(key => ({ header: key.toUpperCase(), key }));
-        sheet.columns = headerCols;
-        headersSet = true;
+        const headerCols = Object.keys(row).map(key => ({ header: key.toUpperCase(), key }))
+        sheet.columns = headerCols
+        headersSet = true
       }
+      if (linhas >= LIMITE) {
+          await sheet.commit()
+          await workbook.commit()
 
-      sheet.addRow(row).commit();
+          parte++
+          linhas = 0
+
+          arquivo = path.join(pasta, `${nomeArquivo}_${parte}.xlsx`)
+          workbook = new ExcelJS.stream.xlsx.WorkbookWriter({ filename: arquivo })
+          sheet = workbook.addWorksheet('Relatório')
+          sheet.columns = headerCols
+      }
+      
+      sheet.addRow(row).commit()
+      linhas++
     }
-    await sheet.commit();
-    await workbook.commit();
+    await sheet.commit()
+    await workbook.commit()
     
     return arquivo
   }
